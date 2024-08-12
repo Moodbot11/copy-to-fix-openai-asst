5d25f6b0dc41dde142be89dcd8eb2662905132c8
Moodbot11 <enigmaitech1@gmail.com>
2024-08-12 15:44:52 -0500

    Update page.tsx

--- app/examples/basic-chat/page.tsx
+++ app/examples/basic-chat/page.tsx
@@ -13,5 +13,31 @@ const Home = () => {
     </main>
   );
 };
+const http = require('https');
 
+const options = {
+	method: 'GET',
+	hostname: 'compare-flight-prices.p.rapidapi.com',
+	port: null,
+	path: '/GetPricesAPI/GetPrices.aspx',
+	headers: {
+		'x-rapidapi-key': '9fbfad16e4mshc8bff8c81c7cdcfp12a178jsnf38a2c8492f2',
+		'x-rapidapi-host': 'compare-flight-prices.p.rapidapi.com'
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on('data', function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on('end', function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
 export default Home;
