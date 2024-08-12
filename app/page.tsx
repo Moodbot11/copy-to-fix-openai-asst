50e92b9ed174f95e5644a4f8290c8b910ff04471
Moodbot11 <enigmaitech1@gmail.com>
2024-08-12 05:48:27 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,17 @@
 
 import React from "react";
 import styles from "./page.module.css";
+import Chat from "./components/chat";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Chat />
+      </div>
+    </main>
+  );
+};
 
 const Home = () => {
   const categories = {
