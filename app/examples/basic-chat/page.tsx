07a944d6451c8ac8a109fea391707bd4d190e010
Moodbot11 <enigmaitech1@gmail.com>
2024-08-14 02:05:37 -0500

    Update page.tsx

--- app/examples/basic-chat/page.tsx
+++ app/examples/basic-chat/page.tsx
@@ -15,17 +15,6 @@ const Home = () => {
 };
 const http = require('https');
 
-const options = {
-	method: 'GET',
-	hostname: 'compare-flight-prices.p.rapidapi.com',
-	port: null,
-	path: '/GetPricesAPI/GetPrices.aspx',
-	headers: {
-		'x-rapidapi-key': '9fbfad16e4mshc8bff8c81c7cdcfp12a178jsnf38a2c8492f2',
-		'x-rapidapi-host': 'compare-flight-prices.p.rapidapi.com'
-	}
-};
-
 const req = http.request(options, function (res) {
 	const chunks = [];
 
